c95766cfa49c48d6518495c25db3fb99e6cebb0d
Devraj <imdev8252@gmail.com>
2025-08-07 14:05:13 +0530

    fix: Update background pattern syntax for consistency in login and register pages

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -116,7 +116,7 @@ export default function LoginPage() {
       <FloatingParticles />
       
       {/* Background Pattern */}
-      <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fillRule="evenodd"%3E%3Cg fill="%23ffffff" fillOpacity="0.05"%3E%3Ccircle cx="30" cy="30" r="2"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-30" />
+      <div className={`absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fillRule="evenodd"%3E%3Cg fill="%23ffffff" fillOpacity="0.05"%3E%3Ccircle cx="30" cy="30" r="2"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-30`} />
 
       <div className="relative z-10 flex min-h-screen">
         {/* Left Side - Features */}
